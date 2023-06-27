82c0788f5588f842bf7953c86f412dcbe4b510cd
dlehdduq22 <dlehdduq22@naver.com>
2023-06-27 18:49:22 +0900

    xml pool 방식

---
+++ java/jdbc/DBcon2.java
@@ -0,0 +1,29 @@
+package jdbc;
+
+import java.sql.Connection;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+
+
+
+public class DBcon2 {
+	private Connection con;
+	
+	public Connection getConnection() {
+		try {
+			InitialContext initctx = new InitialContext();
+			Context ctx = (Context)initctx.lookup("java:comp/env");
+			DataSource source = (DataSource) ctx.lookup("dbcp.mydb");
+			
+			//커넥션 풀을 통해 커넥션 객체 얻기
+			con = source.getConnection();			
+		} catch (Exception e) {
+			e.printStackTrace();	
+		}
+		
+		return con;
+	}
+}
